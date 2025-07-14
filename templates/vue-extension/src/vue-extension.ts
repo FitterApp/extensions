f32b2f8f1960231427b355c96ade0a39ffc1d461
barsbek <mak.bekmamat@gmail.com>
2025-07-14 22:18:50 +0800

    dont forget the provide and update rules for the cursor

--- templates/vue-extension/src/vue-extension.ts
+++ templates/vue-extension/src/vue-extension.ts
@@ -3,8 +3,9 @@ import VueExtension from './VueExtension.vue'
 
 let app: ReturnType<typeof createApp> | null = null
 
-export function mount(el: HTMLElement) {
+export function mount(el: HTMLElement, mvt: any) {
   app = createApp(VueExtension)
+  app.provide('mvt', mvt)
   app.mount(el)
 }
 
